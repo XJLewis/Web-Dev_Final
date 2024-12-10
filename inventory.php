a93c48815ed8cd0b7e961168f3b8d47b4a32837c
Xander Lewis <70913145+XJLewis@users.noreply.github.com>
2024-12-09 21:03:38 -0500

    working on about

--- inventory.php
+++ inventory.php
@@ -107,7 +107,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['delete_item'])) {
 <body>
     <div class="hero-section">
         <h1 class="hero-title">Inventory Management</h1>
-        <h2 class="hero-subtitle">Add to and browse your party's inventory</h2>
+        <h2 class="hero-subtitle">Edit and browse your party's inventory</h2>
         <a href="index.php">
             <button class="btn-about">Home Page</button>
         </a>
